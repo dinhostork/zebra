39ae7e61266a69ef5feb319d890086683be0b7e8
Dinho Stork <dinhostork@live.com>
2024-07-05 21:38:17 -0300

    chore: Add shared package with HandleMessages function for consuming Kafka messages

---
+++ shared/handle_messages.go
@@ -0,0 +1,19 @@
+package shared
+
+import (
+	"log"
+
+	"github.com/IBM/sarama"
+)
+
+// HandleMessages consumes messages from a Kafka partition consumer
+func HandleMessages(partitionConsumer sarama.PartitionConsumer, processMessage func(msg *sarama.ConsumerMessage)) {
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			processMessage(msg)
+		case err := <-partitionConsumer.Errors():
+			log.Printf("Error consuming message: %v", err)
+		}
+	}
+}
