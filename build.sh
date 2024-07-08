45ca8bc02d4ea7c672db3513a06f49748142086e
Dinho Stork <dinhostork@live.com>
2024-07-08 15:53:48 -0300

    feat: Add build script for video_consumer, video_transcode, video_watermark, and main

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build video_consumer
+GOOS=linux GOARCH=amd64 go build -o build/video_consumer ./cmd/video_consumer
+
+# Build video_transcode
+GOOS=linux GOARCH=amd64 go build -o build/video_transcode ./cmd/video_transcode
+
+# Build video_watermark
+GOOS=linux GOARCH=amd64 go build -o build/video_watermark ./cmd/video_watermark
+
+# Build main
+GOOS=linux GOARCH=amd64 go build -o build/main .
