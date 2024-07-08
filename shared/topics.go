13b9cd93b3390f50163d42580aa77e8863187d8d
Dinho Stork <dinhostork@live.com>
2024-07-08 15:17:37 -0300

    refactor: Add SUCCESS_TOPIC and ERROR_TOPIC constants
    This commit adds the SUCCESS_TOPIC and ERROR_TOPIC constants to the shared/topics.go file. These constants are used to define the topics for successful and error messages in the application. By adding these constants, we can ensure consistent and clear communication between different parts of the system when handling video processing and transcoding.

--- shared/topics.go
+++ shared/topics.go
@@ -3,3 +3,5 @@ package shared
 const VIDEO_UPLOAD_TOPIC = "video-upload"
 const VIDEO_TRANSCODE_TOPIC = "video-transcode"
 const VIDEO_WATERMARK_TOPIC = "video-watermark"
+const SUCCESS_TOPIC = "success"
+const ERROR_TOPIC = "error"
