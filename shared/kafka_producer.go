877f25106f0ef3daa449c1541d5ea69bb5b5a9c3
Dinho Stork <dinhostork@live.com>
2024-07-10 21:37:54 -0300

    refactor: Update Kafka producer initialization and add mock support
    This commit updates the InitKafkaProducer function in kafka_producer.go to check for a mock producer and return it if available. This allows for easier testing and mocking of the Kafka producer. Additionally, a new SetKafkaProducer function is added to set the mock producer, enabling the injection of a mock producer for testing purposes. These changes improve the flexibility and testability of the Kafka producer functionality.

--- shared/kafka_producer.go
+++ shared/kafka_producer.go
@@ -1,15 +1,25 @@
 package shared
 
 import (
+	"os"
+
 	"github.com/IBM/sarama"
 )
 
+var mockProducer sarama.SyncProducer
+
 func InitKafkaProducer(topic string) (sarama.SyncProducer, error) {
+	LoadEnv()
+	kafkaHost := os.Getenv("KAFKA_HOST")
+	kafaPort := os.Getenv("KAFKA_PORT")
+
+	if mockProducer != nil {
+		return mockProducer, nil
+	}
+
 	config := sarama.NewConfig()
 	config.Producer.Return.Successes = true
-
-	brokers := []string{"localhost:9092"}
-	producer, err := sarama.NewSyncProducer(brokers, config)
+	producer, err := sarama.NewSyncProducer([]string{kafkaHost + ":" + kafaPort}, config)
 	if err != nil {
 		return nil, err
 	}
@@ -28,4 +38,8 @@ func InitKafkaConsumer() (sarama.Consumer, error) {
 	}
 
 	return consumer, nil
+}
+
+func SetKafkaProducer(producer sarama.SyncProducer) {
+	mockProducer = producer
 }
