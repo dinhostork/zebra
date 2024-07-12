59def081c8d11f51c2f4d896076d3f0d539a7aa1
Dinho Stork <dinhostork@live.com>
2024-07-11 22:49:27 -0300

    refactor: Update video_transcode test to use shared constant

--- internal/video_transcode/transcode_test.go
+++ internal/video_transcode/transcode_test.go
@@ -1,45 +1,10 @@
 package video_transcode_test
 
 import (
-	"log"
-	"os"
 	"testing"
-	appmock "zebra/pkg/mock"
 	"zebra/pkg/tests"
-
-	"github.com/IBM/sarama"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestVideoTranscode(t *testing.T) {
-	const (
-		videoTranscodeTopic = "video.transcode.topic"
-		videoUploadTopic    = "video.upload.topic"
-	)
-
-	// Open the test video file
-	file, err := os.Open("../../testdata/" + tests.VIDEO_TEST_FILE_NAME)
-	if err != nil {
-		t.Fatalf("Failed to open test video file: %v", err)
-	}
-	defer file.Close()
-
-	tests.VideoConsumerTst(t)
-
-	// Set up mock Kafka for video transcode
-	consumer, _, _ := appmock.SetupMockKafka(t, videoTranscodeTopic, "test_key", tests.VIDEO_TEST_FILE_NAME)
-
-	// Consume the message from the video transcode topic
-	partition, err := consumer.ConsumePartition(videoTranscodeTopic, 0, sarama.OffsetOldest)
-	if err != nil {
-		t.Fatalf("Failed to consume partition: %v", err)
-	}
-	defer partition.Close()
-
-	// Read the message from the partition
-	msg := <-partition.Messages()
-
-	// Assert that the video file is the same as the test video file
-	log.Printf("Received video file: %s\n", string(msg.Value))
-	assert.Equal(t, tests.VIDEO_TEST_FILE_NAME, string(msg.Value))
+	tests.VideoTranscodeTst(t)
 }
---
+++ pkg/tests/make_video_watermark_tst.go
@@ -0,0 +1,38 @@
+package tests
+
+import (
+	"log"
+	"testing"
+	appmock "zebra/pkg/mock"
+
+	"github.com/IBM/sarama"
+	"github.com/stretchr/testify/assert"
+)
+
+func VideoWatermarkTst(t *testing.T) {
+
+	const (
+		videoWatermarkTopic = "video.watermark.topic"
+		videoUploadTopic    = "video.upload.topic"
+	)
+
+	VideoTranscodeTst(t)
+
+	// Set up mock Kafka for video watermark
+	consumer, _, _ := appmock.SetupMockKafka(t, videoWatermarkTopic, "test_key", VIDEO_TEST_FILE_NAME)
+
+	// Consume the message from the video watermark topic
+	partition, err := consumer.ConsumePartition(videoWatermarkTopic, 0, sarama.OffsetOldest)
+	if err != nil {
+		t.Fatalf("Failed to consume partition: %v", err)
+	}
+	defer partition.Close()
+
+	// Read the message from the partition
+	msg := <-partition.Messages()
+
+	// Assert that the video file is the same as the test video file
+	log.Printf("Received video file: %s\n", string(msg.Value))
+	assert.Equal(t, VIDEO_TEST_FILE_NAME, string(msg.Value))
+
+}
