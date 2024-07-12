d24b0f4ff588ef2289d662e46d4a70f451c1e9d7
Dinho Stork <dinhostork@live.com>
2024-07-11 22:25:52 -0300

    refactor: Update video_consumer test use shared module

--- internal/video_consumer/consumer_test.go
+++ internal/video_consumer/consumer_test.go
@@ -2,25 +2,9 @@ package video_consumer_test
 
 import (
 	"testing"
-	"zebra/internal/video_consumer"
-	appmock "zebra/pkg/mock"
-
-	"github.com/IBM/sarama"
-	"github.com/stretchr/testify/assert"
+	"zebra/pkg/tests"
 )
 
 func TestVideoConsumer(t *testing.T) {
-	const videoUploadTopic = "video.upload.topic"
-
-	appmock.SetupMockKafka(t, videoUploadTopic, "test_key", "test_video_file.mp4")
-
-	testMessage := &sarama.ConsumerMessage{
-		Value: []byte("test_video_file.mp4"),
-	}
-
-	// Simulate the consumer handling the message
-	handler := &video_consumer.DefaultMessageHandler{}
-	lastMsg := handler.HandleMessage(testMessage)
-	// Verify the handler processed the message correctly
-	assert.Equal(t, string(testMessage.Value), string(lastMsg.Value), "Message value should match")
+	tests.VideoConsumerTst(t)
 }
