3bc82115c3387c9318932e1e7f5086f1c14c3c9f
Dinho Stork <dinhostork@live.com>
2024-07-11 22:26:51 -0300

    refactor: Update video_transcode test to use shared constant

--- internal/video_transcode/transcode_test.go
+++ internal/video_transcode/transcode_test.go
@@ -1,10 +1,11 @@
 package video_transcode_test
 
 import (
+	"log"
 	"os"
 	"testing"
-	"zebra/internal/video_consumer"
 	appmock "zebra/pkg/mock"
+	"zebra/pkg/tests"
 
 	"github.com/IBM/sarama"
 	"github.com/stretchr/testify/assert"
@@ -14,30 +15,19 @@ func TestVideoTranscode(t *testing.T) {
 	const (
 		videoTranscodeTopic = "video.transcode.topic"
 		videoUploadTopic    = "video.upload.topic"
-		videoFileName       = "test_video_file.mp4"
 	)
 
 	// Open the test video file
-	file, err := os.Open("../../testdata/" + videoFileName)
+	file, err := os.Open("../../testdata/" + tests.VIDEO_TEST_FILE_NAME)
 	if err != nil {
 		t.Fatalf("Failed to open test video file: %v", err)
 	}
 	defer file.Close()
 
-	// Set up mock Kafka for video upload
-	appmock.SetupMockKafka(t, videoUploadTopic, "test_key", file.Name())
-
-	// Create a video consumer message
-	video := &sarama.ConsumerMessage{
-		Value: []byte(file.Name()),
-	}
-
-	// Handle the video message
-	videoHandler := &video_consumer.DefaultMessageHandler{}
-	videoHandler.HandleMessage(video)
+	tests.VideoConsumerTst(t)
 
 	// Set up mock Kafka for video transcode
-	consumer, _, _ := appmock.SetupMockKafka(t, videoTranscodeTopic, "test_key", videoFileName)
+	consumer, _, _ := appmock.SetupMockKafka(t, videoTranscodeTopic, "test_key", tests.VIDEO_TEST_FILE_NAME)
 
 	// Consume the message from the video transcode topic
 	partition, err := consumer.ConsumePartition(videoTranscodeTopic, 0, sarama.OffsetOldest)
@@ -50,5 +40,6 @@ func TestVideoTranscode(t *testing.T) {
 	msg := <-partition.Messages()
 
 	// Assert that the video file is the same as the test video file
-	assert.Equal(t, videoFileName, string(msg.Value))
+	log.Printf("Received video file: %s\n", string(msg.Value))
+	assert.Equal(t, tests.VIDEO_TEST_FILE_NAME, string(msg.Value))
 }
